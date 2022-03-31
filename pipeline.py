882df8ca06f7aec0c50725d52caad44c4b28c836
ransela17 <ransela17@gmail.com>
2022-03-31 19:18:15 +0300

    pipeline file

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,13 @@
 #from RESTClinet import RestClient
-import requests, json
+import json
 from configParser import ConfigParser
-from Auth import Auth
 from RESTClient import RESTClient
 
 class Pipeline:
 
     def __init__(self):
         self.data = ConfigParser('config.yaml','pipeline').get_data()
-        # self.integrations = data['integrations']
+        self.integrations = self.data['integrations']
         # self.integrations = data['integrations']
 
     def create_inegrations(self):     
@@ -16,19 +15,27 @@ class Pipeline:
         api_path = 'pipeline/api/v1/projectIntegrations'
         self.integrations = self.data['integrations']
         for integration in self.integrations:
-            payload = json.load(integration['masterIntegrationName']) 
-            response = RESTClient().api_call(http_method, api_path, payload)
+            print (integration)
+            # payload = json.load(integration['masterIntegrationName']) 
+            # response = RESTClient().api_call(http_method, api_path, payload)
 
     def create_pipeline_source(self):
         http_method = 'POST' 
         api_path = 'pipeline/api/v1/pipelinesources'
         self.source = self.data['source']
         for integration in self.integrations:
-            payload = json.load(integration['masterIntegrationName']) 
-            response = RESTClient().api_call(http_method, api_path, payload)
+            print(integration)
+            payload = json.dump(integration['masterIntegrationName']) 
+            # response = RESTClient().api_call(http_method, api_path, payload)
 
     def trigger_pipeline():
         pass
         
     def report_status():
         pass
+
+    
+if __name__ == "__main__":
+    c1 = Pipeline().create_pipeline_source()
+    print(c1)
+    
