a7a2e69fc8762e79aee2ffc846e73e13923f934d
saarm <saarm@jfrog.com>
2022-03-31 14:50:53 +0300

    Authentication and config parsing module

--- Auth.py
+++ Auth.py
@@ -1,57 +1,39 @@
-# import requests
+import requests, json
+from configParser import ConfigParser
+from cryptography.fernet import Fernet
 
-# username = 'adirgali2@gmail.com'
-# password = 'P@P@9ol.poi'
+class Auth:
+    def __init__(self):
+        self.token = None
+        config = ConfigParser("config.yaml","authentication").get_data()
+        self.user = config['user']
+        self.password = config['password']
+        self.base_url = config['url']
 
-# #r = requests.post('https://galipapa.jfrog.io/artifactory/api/security/token',data='username= adirgali2@gmail.com&password= P@P@9ol.poi')
+    # Create Token For JPD
+    def create_token(self):
+        api_path = 'artifactory/api/security/token'
+        url = f"https://{self.base_url}/{api_path}"
+        header = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = f"username={self.user}&scope=member-of-groups:administrators&expires_in=3600"
+        auth = f"{self.user}",f"{self.password}"
+        response = requests.post(url=url, headers=header, data=data, auth=auth).json()
+        access_token = response['access_token']
 
-# #r = requests.post("https://galipapa.jfrog.io/artifactory/api/security/token",data={'username': 'adirgali2@gmail.com', 'password': 'P@P@9ol.poi'})
+        key = Fernet.generate_key()
+        encryption_type = Fernet(key)
+        token_encoded = bytes(access_token,'UTF-8')
+        encrypted_token = encryption_type.encrypt(token_encoded)
+        self.token = encrypted_token
+        self.key = key
+    
+    def get_token(self):
+        if self.token is None:
+            self.create_token()
+        encryption_type = Fernet(self.key)
+        decrypted_token = encryption_type.decrypt(self.token)
+        return decrypted_token.decode('utf-8')
 
-# #curl -s -XPOST -u $username:$password https://galipapa.jfrog.io/artifactory/api/security/token -d "username=$username" -d "scope=member-of-groups:administrators"
-
-# data = {
-#     'j_username': 'adirgali2@gmail.com',
-#     'j_password': 'P@P@9ol.poi'
-# }
-
-# response = requests.post(
-#     'https://galipapa.jfrog.io/artifactory/api/security/token',
-#     data=data
-# )
-
-
-# print(response.text)
- 
-
-
-
-# #curl -uadmin:password -XPOST "http://localhost:8081/artifactory/api/security/token" -d "username=johnq" -d "scope=member-of-groups:readers"
-
-# from urllib import response
-# import requests
-# from requests.auth import HTTPBasicAuth
-
-# r = requests.post('https://galipapa.jfrog.io/artifactory/api/security/token', auth=HTTPBasicAuth('adirgali2@gmail.com', 'P@P@9ol.poi'))
-
-# print(r.text)
-#from urllib import response
-
-import requests
-import json
-
-header = {'Content-Type': 'application/x-www-form-urlencoded'}
-
-data = 'username=,&scope=member-of-groups:administrators'
-
-auth =','
-
-response = requests.post('https://galipapa.jfrog.io/artifactory/api/security/token', headers=header, data=data, auth=auth).json()
-
-
-f = open("config.txt", "w")
-
-f.write(response['access_token'])
-f.close()
-
-
-
+if __name__ == '__main__':
+    t = Auth().get_token()
+    print(t)
--- configParser.py
+++ configParser.py
@@ -1,20 +1,23 @@
 import yaml
-from os.path import expanduser
-
+import os
 
 class ConfigParser:
     """Get specific config data for your application
-       Create an instance, give 2 params - yaml_name & app name(file name)
+       Create an instance, give 2 params - file_name & app name(file name)
        Put the YAML file inside 'jpd' folder in your home directory """
 
-    def __init__(self, yaml_name, app):
+    def __init__(self, file_name, app, file_path = None):
         self.yaml_data = {}
-        self.yaml_name = yaml_name
+        self.file_name = file_name
         self.app = app
+        self.file_path = file_path
+
 
     def read_file(self):
-        file_path = expanduser(f"~/jpd/{self.yaml_name}")
-        with open(file_path, 'r') as stream:
+        if self.file_path is None:
+            self.file_path = os.path.expanduser("~/jpd/")
+        file = os.path.join(self.file_path,self.file_name)
+        with open(file, 'r') as stream:
             try:
                 self.yaml_data = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
@@ -23,13 +26,18 @@ class ConfigParser:
     def get_data(self):
         self.read_file()
         try:
-            return self.yaml_data[self.app]
+            data = self.yaml_data[self.app]
+            return data
         except BaseException as err:
-            raise BaseException(f"Can't find application: {err} in the {self.yaml_name} file")
+            raise BaseException(f"Can't find application: {err} in the {self.file_name} file")
+        except KeyError:
+            print(f"Are you sure {self.app} is the correct application? Not found in the config file")
+            raise KeyError
 
-
-c1 = ConfigParser(yaml_name='Test.yml', app='xray')
-print(c1.get_data())
+if __name__ == "__main__":
+    # c1 = ConfigParser(file_name='Test.yml', app='xray')
+    # print(c1.get_data())
+    pass
 
 
 
