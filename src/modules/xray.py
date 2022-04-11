e01fcfda350e4c7d1f102ee2c0289f3da2b9f2ee
Kobi <wkobiw@gmail.com>
2022-04-11 18:32:42 +0300

    Fix self bug

--- src/modules/xray.py
+++ src/modules/xray.py
@@ -4,21 +4,26 @@ import json
 import click
 import os
 
+app_name = os.path.basename(__file__).split('.')[0].lower()
+xray_data = ConfigParser(file_name='config.yaml', app=app_name).get_data()
+
 
 class Xray:
 
     def __init__(self):
-        self.app_name = os.path.basename(__file__).split('.')[0].lower()
-        self.xray_data = ConfigParser(file_name='config.yaml', app=self.app_name).get_data()
+        pass
+        # self.app_name = os.path.basename(__file__).split('.')[0].lower()
+        # self.xray_data = ConfigParser(file_name='config.yaml', app=self.app_name).get_data()
 
+    @staticmethod
     @click.command()
-    def create_policy(self):
+    def create_policy():
         """
         :input: client and the data about the policies
 
         :return: Creates a new policy , with the data that the user had submitted in the file
         """
-        polices_list = self.xray_data["policies"]
+        polices_list = xray_data["policies"]
         api_path = "xray/api/v2/policies"
         for policy_data_block in polices_list:
             policy_data = (next(iter(policy_data_block.values())))
@@ -26,14 +31,15 @@ class Xray:
             resp = RESTClient(api_path=api_path, http_method="POST", data=repo_json).api_call()
             click.echo(resp.text)
 
+    @staticmethod
     @click.command()
-    def create_watch(self):
+    def create_watch():
         """
         :input: client and the data about the watches
 
         :return: Creates a new watch , with the data that the user had submitted in the file
         """
-        watches_list = self.xray_data["watches"]
+        watches_list = xray_data["watches"]
         api_path = "xray/api/v2/watches"
         for watch_data_block in watches_list:
             watch_data = (next(iter(watch_data_block.values())))
